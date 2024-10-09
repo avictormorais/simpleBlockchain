5d8b68ece27b4140690c7115609a33931b81c56b
avictormorais <victormm.dev@gmail.com>
2024-10-09 09:46:28 -0300

    add: create basic version of the blockchain

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
 const sha256 = require('js-sha256');
 
-console.log(sha256('Testando hash'))
+class Blockchain {
+
+    constructor() {
+        this.chain = [];
+        this.Transactions = [];
+        this.newBlock();
+    }
+
+    hash(block){
+        let { hash, ...blockToHash } = block;
+        return sha256(JSON.stringify(blockToHash));
+    }
+
+    getLastBlock(){
+        return this.chain[this.chain.length-1]
+    }
+
+    newBlock(){
+        let newBlock = {
+            timestamp: Date.now(),
+            transactions: this.Transactions,
+            previousHash: this.getLastBlock() ? this.hash(this.getLastBlock()) : '0'
+        }
+        newBlock.hash = this.hash(newBlock)
+        this.chain.push(newBlock);
+        this.Transactions = [];
+        return newBlock;
+    }
+
+    createTransaction(sender, receiver, tokenAmount){
+        this.Transactions.push({
+            sender: sender,
+            receiver: receiver,
+            tokenAmount: tokenAmount
+        })
+    }
+
+    isBlockchainValid(){
+        for (let i = 1; i < this.chain.length; i++) {
+            let currentBlock = this.chain[i];
+            let previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== this.hash(currentBlock)) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+}
+
+let BLOCKCHAIN = new Blockchain();
+
+BLOCKCHAIN.createTransaction("Victor", "Davi", 7)
+
+BLOCKCHAIN.newBlock();
+
+console.log(`A blockchain é válida? ${BLOCKCHAIN.isBlockchainValid() ? "Sim" : "Não"}`)
